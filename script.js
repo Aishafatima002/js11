1669db7f49483693d8c66e0356f529acae1dc48c
Aisha <fatimaaisha002@gmail.com>
2024-10-11 17:44:10 +0500

    n

---
+++ script.js
@@ -0,0 +1,15 @@
+//primitive data
+let num1=11
+let num2=num1
+console.log(num1);
+console.log(num2);
+num1++;
+//non primitive
+
+let a=[1,2,3]
+let b=a;
+console.log(a)
+console.log(b);
+a.push(4)
+console.log(a);
+console.log(b);
